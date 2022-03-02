af86d9a5feb443fe18c51c36b7e0e1876ce9fa61
Mohamed Es-saadani <essaadani80@gmail.com>
2022-03-02 08:32:39 +0100

    store

--- src/store/Store.js
+++ src/store/Store.js
@@ -10,7 +10,7 @@ export const useStore = () => useContext(Store);
 export const StoreProvider = ({ children, initialState, reducer }) => {
   const [globalState, dispatch] = React.useReducer(reducer, initialState);
 
-  const logoColor = "#ff0000";
+  // const logoColor = "#ff0000";
 
   return (
     <Store.Provider value={[globalState, dispatch]}>{children}</Store.Provider>
